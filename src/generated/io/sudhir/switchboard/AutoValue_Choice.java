c87a73d84522c8468a5fe9af91ca718ce0205ea9
Sudhir Jonathan <sudhir.j@gmail.com>
2017-08-13 19:32:46 +0530

    Added guava with sources made the config platform independent.

--- src/generated/io/sudhir/switchboard/AutoValue_Choice.java
+++ src/generated/io/sudhir/switchboard/AutoValue_Choice.java
@@ -4,75 +4,75 @@ package io.sudhir.switchboard;
 import javax.annotation.Generated;
 
 @Generated("com.google.auto.value.processor.AutoValueProcessor")
-final class AutoValue_Choice extends Choice {
+ final class AutoValue_Choice extends Choice {
 
-    private final Supply supply;
-    private final Demand demand;
-    private final int score;
+  private final Supply supply;
+  private final Demand demand;
+  private final int score;
 
-    AutoValue_Choice(
-            Supply supply,
-            Demand demand,
-            int score) {
-        if (supply == null) {
-            throw new NullPointerException("Null supply");
-        }
-        this.supply = supply;
-        if (demand == null) {
-            throw new NullPointerException("Null demand");
-        }
-        this.demand = demand;
-        this.score = score;
+  AutoValue_Choice(
+      Supply supply,
+      Demand demand,
+      int score) {
+    if (supply == null) {
+      throw new NullPointerException("Null supply");
     }
-
-    @Override
-    public Supply supply() {
-        return supply;
+    this.supply = supply;
+    if (demand == null) {
+      throw new NullPointerException("Null demand");
     }
+    this.demand = demand;
+    this.score = score;
+  }
 
-    @Override
-    public Demand demand() {
-        return demand;
-    }
+  @Override
+  public Supply supply() {
+    return supply;
+  }
 
-    @Override
-    public int score() {
-        return score;
-    }
+  @Override
+  public Demand demand() {
+    return demand;
+  }
 
-    @Override
-    public String toString() {
-        return "Choice{"
-                + "supply=" + supply + ", "
-                + "demand=" + demand + ", "
-                + "score=" + score
-                + "}";
-    }
+  @Override
+  public int score() {
+    return score;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == this) {
-            return true;
-        }
-        if (o instanceof Choice) {
-            Choice that = (Choice) o;
-            return (this.supply.equals(that.supply()))
-                    && (this.demand.equals(that.demand()))
-                    && (this.score == that.score());
-        }
-        return false;
-    }
+  @Override
+  public String toString() {
+    return "Choice{"
+        + "supply=" + supply + ", "
+        + "demand=" + demand + ", "
+        + "score=" + score
+        + "}";
+  }
 
-    @Override
-    public int hashCode() {
-        int h = 1;
-        h *= 1000003;
-        h ^= this.supply.hashCode();
-        h *= 1000003;
-        h ^= this.demand.hashCode();
-        h *= 1000003;
-        h ^= this.score;
-        return h;
+  @Override
+  public boolean equals(Object o) {
+    if (o == this) {
+      return true;
     }
+    if (o instanceof Choice) {
+      Choice that = (Choice) o;
+      return (this.supply.equals(that.supply()))
+           && (this.demand.equals(that.demand()))
+           && (this.score == that.score());
+    }
+    return false;
+  }
+
+  @Override
+  public int hashCode() {
+    int h = 1;
+    h *= 1000003;
+    h ^= this.supply.hashCode();
+    h *= 1000003;
+    h ^= this.demand.hashCode();
+    h *= 1000003;
+    h ^= this.score;
+    return h;
+  }
 
 }
--- test/generated/io/sudhir/switchboard/AutoValue_ConstantDemand.java
+++ test/generated/io/sudhir/switchboard/AutoValue_ConstantDemand.java
@@ -4,48 +4,48 @@ package io.sudhir.switchboard;
 import javax.annotation.Generated;
 
 @Generated("com.google.auto.value.processor.AutoValueProcessor")
-final class AutoValue_ConstantDemand extends ConstantDemand {
+ final class AutoValue_ConstantDemand extends ConstantDemand {
 
-    private final String type;
+  private final String type;
 
-    AutoValue_ConstantDemand(
-            String type) {
-        if (type == null) {
-            throw new NullPointerException("Null type");
-        }
-        this.type = type;
+  AutoValue_ConstantDemand(
+      String type) {
+    if (type == null) {
+      throw new NullPointerException("Null type");
     }
+    this.type = type;
+  }
 
-    @Override
-    String type() {
-        return type;
-    }
+  @Override
+  String type() {
+    return type;
+  }
 
-    @Override
-    public String toString() {
-        return "ConstantDemand{"
-                + "type=" + type
-                + "}";
-    }
+  @Override
+  public String toString() {
+    return "ConstantDemand{"
+        + "type=" + type
+        + "}";
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == this) {
-            return true;
-        }
-        if (o instanceof ConstantDemand) {
-            ConstantDemand that = (ConstantDemand) o;
-            return (this.type.equals(that.type()));
-        }
-        return false;
+  @Override
+  public boolean equals(Object o) {
+    if (o == this) {
+      return true;
     }
+    if (o instanceof ConstantDemand) {
+      ConstantDemand that = (ConstantDemand) o;
+      return (this.type.equals(that.type()));
+    }
+    return false;
+  }
 
-    @Override
-    public int hashCode() {
-        int h = 1;
-        h *= 1000003;
-        h ^= this.type.hashCode();
-        return h;
-    }
+  @Override
+  public int hashCode() {
+    int h = 1;
+    h *= 1000003;
+    h ^= this.type.hashCode();
+    return h;
+  }
 
 }
