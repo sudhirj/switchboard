deacd09cdce51fa7b8c045e53e0606c0b3e809d2
Sudhir Jonathan <sudhir.j@gmail.com>
2017-08-22 23:18:53 +0530

    Refactored

--- src/io/sudhir/switchboard/players/GreedyPlayer.java
+++ src/io/sudhir/switchboard/players/GreedyPlayer.java
@@ -16,11 +16,15 @@ public class GreedyPlayer implements Player {
     @Override
     @Nullable
     public Choice bestChoiceFor(Board board, Goal goal) {
-        return board.availableChoices().parallelStream().max(goal.choiceComparator()).orElse(null);
+        return board.availableChoices().parallelStream()
+                .max(goal.choiceComparator())
+                .orElse(null);
     }
 
     @Override
     public Collection<Choice> goodChoicesFor(Board board, Goal goal) {
-        return board.availableChoices().parallelStream().sorted(goal.choiceComparator().reversed()).limit(3).collect(toImmutableSet());
+        return board.availableChoices().parallelStream()
+                .sorted(goal.choiceComparator().reversed())
+                .limit(3).collect(toImmutableSet());
     }
 }
