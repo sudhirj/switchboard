c3fe9cafeec72eff7e140069a354f3c543d57c18
Sudhir Jonathan <sudhir.j@gmail.com>
2017-08-24 16:01:22 +0530

    Removed old files

---
+++ src/generated/io/sudhir/switchboard/AutoValue_ImmutableChoice.java
@@ -0,0 +1,78 @@
+
+package io.sudhir.switchboard;
+
+import javax.annotation.Generated;
+
+@Generated("com.google.auto.value.processor.AutoValueProcessor")
+ final class AutoValue_ImmutableChoice extends ImmutableChoice {
+
+  private final Supply supply;
+  private final Demand demand;
+  private final int score;
+
+  AutoValue_ImmutableChoice(
+      Supply supply,
+      Demand demand,
+      int score) {
+    if (supply == null) {
+      throw new NullPointerException("Null supply");
+    }
+    this.supply = supply;
+    if (demand == null) {
+      throw new NullPointerException("Null demand");
+    }
+    this.demand = demand;
+    this.score = score;
+  }
+
+  @Override
+  public Supply supply() {
+    return supply;
+  }
+
+  @Override
+  public Demand demand() {
+    return demand;
+  }
+
+  @Override
+  public int score() {
+    return score;
+  }
+
+  @Override
+  public String toString() {
+    return "ImmutableChoice{"
+        + "supply=" + supply + ", "
+        + "demand=" + demand + ", "
+        + "score=" + score
+        + "}";
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == this) {
+      return true;
+    }
+    if (o instanceof ImmutableChoice) {
+      ImmutableChoice that = (ImmutableChoice) o;
+      return (this.supply.equals(that.supply()))
+           && (this.demand.equals(that.demand()))
+           && (this.score == that.score());
+    }
+    return false;
+  }
+
+  @Override
+  public int hashCode() {
+    int h = 1;
+    h *= 1000003;
+    h ^= this.supply.hashCode();
+    h *= 1000003;
+    h ^= this.demand.hashCode();
+    h *= 1000003;
+    h ^= this.score;
+    return h;
+  }
+
+}
